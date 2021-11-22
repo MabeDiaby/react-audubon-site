6dc58668ba64c339d8cf60fbaa3a569980546ab8
Mabe-Diaby <diabymabe@gmail.com>
2021-11-22 18:37:17 -0500

    Step 4

--- src/component/Birds.js
+++ src/component/Birds.js
@@ -1,8 +1,19 @@
 import React, {useState, useEffect} from 'react';
 
 function Birds(props) {
-    const [varible, setVariable] = useState();
+    let url = 'https://audubon-api.herokuapp.com/api/birds'
+
+    const [birds, setBirds] = useState();
     
+useState(() => {
+    fetch(url)
+    .then(res => res.json())
+    .then(json => {
+        // console.log(res);
+  })
+  .catch(console.error);
+    }, [] )
+
     return (
         <div>
             <h1>Some Birds Tweet Tweet</h1>
