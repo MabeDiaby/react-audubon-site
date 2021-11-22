5dc75c0fa21bb7cfd0d3669bc500c05a93b10fcc
Mabe-Diaby <diabymabe@gmail.com>
2021-11-22 18:31:21 -0500

    Step 3

--- src/component/Birds.js
+++ src/component/Birds.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 function Birds(props) {
+    const [varible, setVariable] = useState();
+    
     return (
         <div>
             <h1>Some Birds Tweet Tweet</h1>
