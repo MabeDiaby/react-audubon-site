ae95273e0e860b10e1be92a2a600e53c9a95eaa5
Mabe-Diaby <diabymabe@gmail.com>
2021-11-29 13:08:17 -0500

    Step 5- Display the Birds

--- src/component/Birds.js
+++ src/component/Birds.js
@@ -3,22 +3,37 @@ import React, {useState, useEffect} from 'react';
 function Birds(props) {
     let url = 'https://audubon-api.herokuapp.com/api/birds'
 
-    const [birds, setBirds] = useState();
+    const [birds, setBirds] = useState([]);
     
 useState(() => {
     fetch(url)
     .then(res => res.json())
     .then(json => {
+        setBirds(json)
         // console.log(res);
   })
   .catch(console.error);
     }, [] )
 
     return (
-        <div>
-            <h1>Some Birds Tweet Tweet</h1>
-        </div>
-    );
+      <section className="container">
+      {birds.map(bird => {
+        return (
+          <div className="card">
+            <div className="card-image">
+              <img
+                src={bird.image}
+                alt={bird.name}
+              />
+            </div>
+                      <div className="card-title">
+                          <h3>{bird.name}</h3>
+                      </div>
+                  </div>
+              );
+          })}   
+      </section>
+  );
 }
 
 export default Birds;
